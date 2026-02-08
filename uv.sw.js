527b59865051d7dda521449d08060b9582fca258
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2026-02-07 21:08:20 -0800

    Update and rename uv-sw.js to uv.sw.js

---
+++ uv.sw.js
@@ -0,0 +1,10 @@
+/* Service Worker Bridge */
+importScripts('/data/local/pages/unlocker/ultraviolet/uv.bundle.js');
+importScripts('/data/local/pages/unlocker/ultraviolet/uv.config.js');
+importScripts('/data/local/pages/unlocker/ultraviolet/uv.sw.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(sw.fetch(event));
+});
